5dd1dce09601989a55a6cd7c2127a3ea63b67836
Daniel Boghossian <91652856+Dg155@users.noreply.github.com>
2022-09-08 22:30:32 -0700

    Portfolio done! Modal added

---
+++ myWebsite.js
@@ -0,0 +1,15 @@
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+console.log(open);
+console.log(modal_container);
+console.log(close);
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+})
